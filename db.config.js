3778da34ea048046a069e9035a9e8326d6835d28
temkojan <138794000+temkojan@users.noreply.github.com>
2023-07-06 21:18:15 +0700

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-ci829r98g3n3vm2rl8dg-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "Dt8U2FQPvB2l3Gh0VU8qEcsFJtvtajCU",
+    DB: "temkowidget",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
